f9c613b378d1743dfdada60ab923c4d4a39e84ad
YoussefJell <youssef.jellouli@yahoo.com>
2023-05-09 19:24:29 +0000

    add

---
+++ 0x02-react_intro/task_4/dashboard/src/utils.test.js
@@ -0,0 +1,23 @@
+import { getFullYear } from './utils.js';
+import { getFooterCopy } from './utils.js';
+import { getLatestNotification } from './utils.js';
+
+test('getFullYear returns the correct year', () => {
+  const currentYear = new Date().getFullYear();
+  expect(getFullYear()).toBe(currentYear);
+});
+
+
+test('getFooterCopy returns correct string', () => {
+  const trueCopy = "Holberton School";
+  const falseCopy = "Holberton School main dashboard";
+
+  expect(getFooterCopy(true)).toBe(trueCopy);
+  expect(getFooterCopy(false)).toBe(falseCopy);
+});
+
+
+test('getLatestNotification returns correct string', () => {
+  const latestNotification = getLatestNotification();
+  expect(latestNotification).toContain("<strong>Urgent requirement</strong> - complete by EOD");
+});
