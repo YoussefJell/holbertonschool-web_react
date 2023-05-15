178133f3fe526d70009181114b350661684089aa
YoussefJell <youssef.jellouli@yahoo.com>
2023-05-15 20:06:02 +0000

    add

--- 0x03-react_props/task_4/dashboard/src/Notifications/Notifications.test.js
+++ 0x03-react_props/task_4/dashboard/src/Notifications/Notifications.test.js
@@ -18,4 +18,28 @@ describe("<Notifications />", () => {
   //   wrapper.update();
   //   expect(wrapper.find(".Notifications p").text()).toEqual(text);
   // });
+  it("menu item is being displayed when displayDrawer is false", () => {
+    const wrapper = shallow(<Notifications />);
+    wrapper.update();
+    const item = wrapper.find("div.menuItem");
+    expect(item).toHaveLength(1);
+  });
+  it("div.Notifications is not being displayed when displayDrawer is false", () => {
+    const wrapper = shallow(<Notifications />);
+    wrapper.update();
+    const item = wrapper.find("div.Notifications");
+    expect(item).toHaveLength(0);
+  });
+  it("menu item is being displayed when displayDrawer is true", () => {
+    const wrapper = shallow(<Notifications displayDrawer />);
+    wrapper.update();
+    const item = wrapper.find("div.menuItem");
+    expect(item).toHaveLength(1);
+  });
+  it("div.Notifications is being displayed when displayDrawer is true", () => {
+    const wrapper = shallow(<Notifications displayDrawer />);
+    wrapper.update();
+    const item = wrapper.find("div.Notifications");
+    expect(item).toHaveLength(1);
+  });
 });
